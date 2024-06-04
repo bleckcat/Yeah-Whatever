0d63309e521d8be5741315e679c0236fab8dad42
unknown <juanvitor16@hotmail.com>
2024-06-04 13:31:12 +0100

    Character now can run with animation and side rotation

--- player.lua
+++ player.lua
@@ -1,6 +1,12 @@
 require("animation_core")
 
-player_sprite = love.graphics.newImage("sprites/characters/2 Punk/Punk_idle.png")
+local player_sprites_table = {
+    [1] = love.graphics.newImage("sprites/characters/2 Punk/Punk_idle.png"),
+    [2] = love.graphics.newImage("sprites/characters/2 Punk/Punk_run.png"),
+    [3] = love.graphics.newImage("sprites/characters/2 Punk/Punk_run_attack.png"),
+    [4] = love.graphics.newImage("sprites/characters/2 Punk/Punk_death.png"),
+    [5] = love.graphics.newImage("sprites/characters/2 Punk/Punk_hurt.png")
+}
 
 player = {}
 
@@ -11,9 +17,16 @@ function love.load()
     player.hp = 15
     player.strength = 20
     player.magic = 25
+    player.move_speed = 1
+    player.is_moving = false
 
-    player.img = player_sprite
-    player.animation = animateSprite(player_sprite)
+    player.idle = animateSprite(player_sprites_table[1])
+    player.run = animateSprite(player_sprites_table[2])
+    player.run_attack = animateSprite(player_sprites_table[3])
+    player.death = animateSprite(player_sprites_table[4])
+    player.hurt = animateSprite(player_sprites_table[5])
+
+    player.animation = player.idle
 end
 
 function love.update(dt)
@@ -21,9 +34,36 @@ function love.update(dt)
     if player.animation.currentTime >= player.animation.duration then
         player.animation.currentTime = player.animation.currentTime - player.animation.duration
     end
+
+    player.is_moving = false
+    if love.keyboard.isDown("d") then
+        player.is_moving = true
+        player.animation.side = 1
+        player.x = player.x + player.move_speed
+    end
+    if love.keyboard.isDown("a") then
+        player.is_moving = true
+        player.animation.side = -1
+        player.x = player.x - player.move_speed
+    end
+    if love.keyboard.isDown("w") then
+        player.is_moving = true
+        player.y = player.y - player.move_speed
+    end
+    if love.keyboard.isDown("s") then
+        player.is_moving = true
+        player.y = player.y + player.move_speed
+    end
+
+    if player.is_moving then
+        player.animation = player.run
+    else
+        player.animation = player.idle
+    end
 end
 
 function love.draw()
     local spriteNum = math.floor(player.animation.currentTime / player.animation.duration * #player.animation.quads) + 1
-    love.graphics.draw(player.animation.spriteSheet, player.animation.quads[spriteNum], player.x, player.y, 0, 1, 1, 0, 0, 0, 0)
+    love.graphics.draw(player.animation.spriteSheet, player.animation.quads[spriteNum], player.x, player.y, 0,
+        player.animation.side, 1, 16, 0, 0, 0)
 end
